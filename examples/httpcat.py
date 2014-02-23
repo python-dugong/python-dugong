c97a7ca649f040ee4a91e1e39d1084166ced07e2
Nikolaus Rath <Nikolaus@rath.org>
2014-02-22 17:09:54 -0800

    Added httpcat example.

---
+++ examples/httpcat.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+'''
+Retrieve a list of URLs and print them to stdout.
+'''
+
+import sys
+import os.path
+from io import TextIOWrapper
+import re
+
+# We are running from the dugong source directory, make sure that we use modules
+# from this directory
+if __name__ == '__main__':
+    basedir = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), '..'))
+else:
+    basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if (os.path.exists(os.path.join(basedir, 'setup.py')) and
+    os.path.exists(os.path.join(basedir, 'dugong', '__init__.py'))):
+    sys.path.insert(0, basedir)
+
+# When running from HG repo, enable all warnings
+if os.path.exists(os.path.join(basedir, '.hg')):
+    import warnings
+    warnings.simplefilter('default')
+
+from dugong import HTTPConnection, BUFFER_SIZE
+
+for url in sys.argv[1:]:
+    hit = re.match(r'http://([^/]+)(/.+)$', url)
+    assert hit
+    (host, path) = hit.groups()
+    
+    conn = HTTPConnection(host)
+    conn.send_request('GET', path)
+    resp = conn.read_response()
+    if resp.status != 200:
+        raise SystemExit('%d %s' % (resp.status, resp.reason))
+
+    # Determine if we're reading text or binary data, and (in case of text),
+    # what character set is being used.
+    if 'Content-Type' not in resp.headers:
+        type_ = 'application/octet-stream'
+    else:
+        type_ = resp.headers['Content-Type']
+
+    hit = re.match(r'(.+?)(?:; charset=(.+))?$', type_)
+    if not hit:
+        raise SystemExit('Unable to parse content-type: %s' % type_)
+    if hit.group(2):
+        charset = hit.group(2)
+    elif hit.group(1).startswith('text/'):
+        charset = 'latin1'
+    else:
+        charset = None # binary data
+
+    if charset:
+        instream = TextIOWrapper(conn, encoding=charset)
+        outstream = sys.stdout
+    else:
+        instream = conn
+        # Since we're writing bytes rather than text, we need to bypass
+        # any encoding.
+        outstream = sys.stdout.raw
+
+    while True:
+        buf = instream.read(BUFFER_SIZE)
+        if not buf:
+            break
+        outstream.write(buf)
+        
+        
---
+++ test/test_examples.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+'''
+test_dugong.py - Unit tests for Dugong - run with py.test
+
+Copyright (c) Nikolaus Rath <Nikolaus@rath.org>
+
+This module may be distributed under the terms of the Python Software Foundation
+License Version 2.  The complete license text may be retrieved from
+http://hg.python.org/cpython/file/65f2c92ed079/LICENSE.
+'''
+
+if __name__ == '__main__':
+    import pytest
+    import sys
+    sys.exit(pytest.main([__file__] + sys.argv[1:]))
+
+import subprocess
+import os
+import sys
+
+basename = os.path.join(os.path.dirname(__file__), '..')
+
+def test_httpcat():
+    cmdline = [sys.executable,
+              os.path.join(basename, 'examples', 'httpcat.py'),
+              'http://docs.oracle.com/javaee/7/firstcup/doc/creating-example.htm' ]
+    
+    with open('/dev/null', 'wb') as devnull:
+        subprocess.check_call(cmdline, stdout=devnull)
